919f4a8e974330b6e548086aac842ea40af72262
mstrielnikov <makstr@ukr.net>
2022-04-10 18:50:11 +0300

    Added updated and create handlers

--- controllers/demodeployment_controller.go
+++ controllers/demodeployment_controller.go
@@ -20,23 +20,27 @@ import (
 	"context"
 	"fmt"
 
+	scalev1 "mstrielnikov/k8s-operator-sample/api/v1"
+
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/klog"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
-
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/klog"
-
-	scalev1 "mstrielnikov/k8s-operator-sample/api/v1"
 )
 
 const (
-	MaxReplicasNum int32 = 2
+	MaxReplicasNum           int32 = 2
+	DemoDeploymentController       = "DemoDeploymentController"
 )
 
 var (
-	ScaleError error = fmt.Errorf("Unable to scale Demo Deployment replicas greater then %v", MaxReplicasNum)
+	ErrScale = fmt.Errorf("unable to scale demo deployment replicas greater then %v", MaxReplicasNum)
 )
 
 // DemoDeploymentReconciler reconciles a DemoDeployment object
@@ -62,48 +66,108 @@ func (r *DemoDeploymentReconciler) Reconcile(ctx context.Context, req ctrl.Reque
 	_ = log.FromContext(ctx)
 
 	// Operator logic here
-	var demoDeployment scalev1.DemoDeployment
-
-	if err := r.Get(ctx, req.NamespacedName, &demoDeployment); err != nil {
-		if apierrors.IsNotFound(err) {
-			klog.Error(err, "The scalev1/DemoDeployment object %s is not found", demoDeployment.DemoDeploymentName)
-			return ctrl.Result{}, client.IgnoreNotFound(err)
-		} else {
-			klog.Error(err, "unable to fetch scalev1/demoDeployment")
-			return ctrl.Result{}, err
-		}
+	if err := r.handleCreate(ctx, req); err != nil {
+		return ctrl.Result{}, err
 	}
 
-	klog.Infof("Found scalev1/DemoDeployment object %v", demoDeployment)
-
-	// Handle scale error
-	if *demoDeployment.Spec.Replicas > MaxReplicasNum {
-		klog.Error("Unable scalev1/DemoDeployment larger the ", MaxReplicasNum)
-		return ctrl.Result{}, ScaleError
+	if err := r.handleUpdate(ctx, req); err != nil {
+		return ctrl.Result{}, err
+	} else if apierrors.IsConflict(err) {
+		// The DemoDeployment has been updated since we read it.
+		// Requeue the Pod to try to reconciliate again.
+		return ctrl.Result{}, nil
+	} else if apierrors.IsNotFound(err) {
+		klog.Error(err, "object scalev1/DemoDeployment is not found")
+		return ctrl.Result{}, nil
 	}
 
-	if err := r.Update(ctx, &demoDeployment); err != nil {
-		if apierrors.IsConflict(err) {
-			// The DemoDeployment has been updated since we read it.
-			// Requeue the Pod to try to reconciliate again.
-			return ctrl.Result{Requeue: true}, nil
-		}
-		if apierrors.IsNotFound(err) {
-			klog.Error(err, "unable to fetch scalev1/DemoDeployment object %s", demoDeployment.DemoDeploymentName)
-			return ctrl.Result{Requeue: true}, nil
-		} else {
-			klog.Error(err, "unable to update scalev1/DemoDeployment %s", demoDeployment.DemoDeploymentName)
-			return ctrl.Result{}, err
-		}
-	}
-
-	klog.Infof("Successfully updated DemoDeployment %s", demoDeployment.DemoDeploymentName)
-	return ctrl.Result{}, nil
+	return ctrl.Result{Requeue: true}, nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *DemoDeploymentReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&scalev1.DemoDeployment{}).
+		Owns(&appsv1.Deployment{}).
 		Complete(r)
+}
+
+func (r *DemoDeploymentReconciler) handleUpdate(ctx context.Context, req ctrl.Request) error {
+	var demoDeployment scalev1.DemoDeployment
+	if err := r.Get(ctx, req.NamespacedName, &demoDeployment); err != nil {
+		klog.Error(err, "unable to fetch scalev1/demoDeployment %v", demoDeployment)
+		return err
+	}
+	// Handle scale error
+	if *demoDeployment.Spec.Replicas > MaxReplicasNum {
+		klog.Error(ErrScale, "Unable to create scalev1/DemoDeployment with replicas num larger then", MaxReplicasNum)
+		return ErrScale
+	}
+	if err := r.Update(ctx, &demoDeployment); err != nil {
+		klog.Error(err, "unable to update scalev1/DemoDeployment %s", demoDeployment.Name)
+		return err
+	}
+	klog.Infof("Successfully updated DemoDeployment %s", demoDeployment.Name)
+	return nil
+}
+
+func (r *DemoDeploymentReconciler) handleCreate(ctx context.Context, req ctrl.Request) error {
+	var demoDeployment scalev1.DemoDeployment
+	if err := r.Get(ctx, req.NamespacedName, &demoDeployment); err != nil {
+		klog.Error(err, "unable to fetch scalev1/demoDeployment %v", demoDeployment)
+		return err
+	}
+	// Handle scale error
+	if *demoDeployment.Spec.Replicas > MaxReplicasNum {
+		klog.Error(ErrScale, "Unable to create scalev1/DemoDeployment with replicas num larger then", MaxReplicasNum)
+		return ErrScale
+	}
+	deployment := newDemoDeployment(demoDeployment)
+	if err := r.Create(ctx, &deployment); err != nil {
+		klog.Error(err, "Failed to create new Deployment ", "Deployment.Namespace ", deployment.Namespace, "Deployment.Name ", deployment.Name)
+		return err
+	}
+	klog.Infof("Found scalev1/DemoDeployment object %v", deployment)
+	return nil
+}
+
+func newDemoDeployment(demoDeployment scalev1.DemoDeployment) appsv1.Deployment {
+	labels := map[string]string{
+		"app":        demoDeployment.Name,
+		"controller": DemoDeploymentController,
+	}
+	return appsv1.Deployment{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Deployment",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      demoDeployment.Name,
+			Labels:    labels,
+			Namespace: demoDeployment.Namespace,
+			OwnerReferences: []metav1.OwnerReference{
+				*metav1.NewControllerRef(&demoDeployment, schema.GroupVersionKind{
+					Group:   scalev1.GroupVersion.Group,
+					Version: scalev1.GroupVersion.Version,
+					Kind:    demoDeployment.Kind,
+				}),
+			},
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: demoDeployment.Spec.Replicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: labels,
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "pod" + demoDeployment.Name,
+					Namespace: demoDeployment.Namespace,
+					Labels:    labels,
+					// OwnerReferences:            []metav1.OwnerReference{},
+				},
+				Spec: demoDeployment.Spec.Template.Spec,
+			},
+		},
+		Status: appsv1.DeploymentStatus{},
+	}
 }
