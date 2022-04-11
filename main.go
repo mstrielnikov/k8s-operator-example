99209e410a17bbce97c44e6ee06ca13a13e6b799
mstrielnikov <makstr@ukr.net>
2022-04-12 00:33:28 +0300

    Bootstrap webhook

--- main.go
+++ main.go
@@ -84,6 +84,10 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "DemoDeployment")
 		os.Exit(1)
 	}
+	if err = (&scalev1.DemoDeployment{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "DemoDeployment")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
